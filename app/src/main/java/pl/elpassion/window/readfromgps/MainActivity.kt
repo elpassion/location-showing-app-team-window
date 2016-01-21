fc316644d748184fc27c71b76653eabff7b177eb
dmalantowicz <dominik.malantowicz@elpassion.pl>
2016-01-21 14:29:54 +0100

    Working two views with asking for permissions

--- app/src/main/java/pl/elpassion/window/readfromgps/MainActivity.kt
+++ app/src/main/java/pl/elpassion/window/readfromgps/MainActivity.kt
@@ -1,19 +1,92 @@
 package pl.elpassion.window.readfromgps
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.View
-import android.view.Menu
-import android.view.MenuItem
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
+
+    companion object {
+        private val readLocationFromGPSCode = 1
+    }
+
+    val button: Button by lazy { findViewById(R.id.show_location_button) as Button }
+    var longitude: Double = 0.0
+    var latitude: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            showLocationViewIfPermissionGranted()
+        }
+    }
+
+    private fun showLocationViewIfPermissionGranted() {
+        if (isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            startLocationListener()
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                MapViewActivity.start(this)
+            } else {
+                requestPermission(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
+            }
+        }
+    }
+
+    private fun isPermissionGranted(permission: String): Boolean {
+        return PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, permission)
+    }
+
+    private fun requestPermission(permissions: Array<String>) {
+        ActivityCompat.requestPermissions(this, permissions, readLocationFromGPSCode)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>?, grantResults: IntArray) {
+        if (readLocationFromGPSCode == requestCode) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationListener()
+            }
+        }
+    }
+
+    private fun startLocationListener() {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0F, this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        requestPermission(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
+    }
+
+    override fun onLocationChanged(location: Location) {
+        latitude = location.latitude
+        longitude = location.longitude
+        MapViewActivity.start(this, latitude, longitude)
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager.removeUpdates(this)
     }
 
 }
---
+++ app/src/main/java/pl/elpassion/window/readfromgps/MapViewActivity.kt
@@ -0,0 +1,54 @@
+package pl.elpassion.window.readfromgps
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+
+/**
+ * Created by dmalantowicz on 21.01.2016.
+ */
+class MapViewActivity : AppCompatActivity(){
+
+    companion object {
+        private val latitudeKey = "latitudeKey"
+        private val longitudeKey = "longitudeKey"
+        private val messageText = "This is what you are going to see, if you will give this app permission to access GPS"
+        private val latitudeWarsaw: Double = 52.232311
+        private val longitudeWarsaw: Double = 20.984209
+
+        fun start(context : Context, latitude :Double, longitude : Double) {
+            val intent = Intent(context, MapViewActivity::class.java)
+            intent.putExtra(latitudeKey, latitude)
+            intent.putExtra(longitudeKey, longitude)
+            context.startActivity(intent)
+        }
+
+        fun start(activity: Activity) {
+            val intent = Intent(activity, MapViewActivity::class.java)
+            activity.startActivityForResult(intent, 1)
+        }
+    }
+
+    val image : ImageView by lazy { findViewById(R.id.imageView) as ImageView }
+    val message : TextView by lazy { findViewById(R.id.message) as TextView }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.map_layout)
+        val latitude = intent.getDoubleExtra(latitudeKey, latitudeWarsaw )
+        val longitude = intent.getDoubleExtra(longitudeKey, longitudeWarsaw )
+        Glide.with(image.context)
+                .load("https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=10&size=600x300&key=AIzaSyC8Cl3TYbzkZ6bb8_fwKeMhFvx_Be6B0CY")
+                .into(image)
+        if (isMessageToUserRequired()){
+            message.text = messageText
+        }
+    }
+
+    private fun isMessageToUserRequired() = !intent.hasExtra(latitudeKey)
+}
